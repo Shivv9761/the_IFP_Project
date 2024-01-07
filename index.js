9bb4787f42e1a4b054cc88a01b57a870090f80bd
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2024-01-07 07:47:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,7 @@ app.use('/v1/member', memberRoutes)
 app.use('/v1/role', roleRoutes)
 
 const PORT = process.env.PORT || 8080;
-app.get("/", (req,res)=>{
-  return res.json({
-      succeess:true,
-      message:"your server is up and running"
-  })
-})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
